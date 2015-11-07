6bb23da0b47f3bb2762acb93f26e8236061216db
Nick Baicoianu <nick@meanfreepath.com>
2015-11-06 17:27:46 -0800

    added optional caching of Overpass results

--- src/Overpass/ApiClient.java
+++ src/Overpass/ApiClient.java
@@ -1,17 +1,16 @@
 package Overpass;
 
-import com.oracle.javafx.jmx.json.JSONFactory;
-import com.oracle.javafx.jmx.json.JSONReader;
-import com.sun.deploy.net.HttpRequest;
-import jdk.nashorn.internal.parser.JSONParser;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.io.*;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
 import java.nio.charset.Charset;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 import java.util.HashMap;
 
 /**
@@ -21,14 +20,14 @@ import java.util.HashMap;
 public class ApiClient {
     public final static int CONNECT_TIMEOUT = 5000, READ_TIMEOUT = 25000;
     public final static String ENDPOINT_URL = "http://overpass-api.de/api/interpreter";
-    private final static String QUERY_TEMPLATE = "[out:%s];%sout body;", GEOJSON_QUERY_TEMPLATE = "[out:%s];%sout body geom;";
+    private final static String QUERY_TEMPLATE = "[out:%s];%sout meta;", GEOJSON_QUERY_TEMPLATE = "[out:%s];%sout body geom;";
 
     public enum ResponseFormat {
         json, xml
     }
 
     public ResponseFormat responseFormat = ResponseFormat.json;
-    public boolean debug = false;
+    public boolean debug = false, cachingEnabled = true;
     public int status;
 
     public ApiClient(String[] args, HashMap<String, String> kwArgs) {
@@ -51,29 +50,54 @@ public class ApiClient {
     /**
      * Pass in an Overpass query in Overpass QL
      */
-    public Object get(String query, boolean asGeoJSON) throws Exceptions.UnknownOverpassError {
+    public JSONArray get(String query, boolean asGeoJSON) throws Exceptions.UnknownOverpassError {
         String fullQuery = constructQLQuery(query, asGeoJSON);
+
+        File cacheFile = null;
+        if(cachingEnabled) {
+            try {
+                final MessageDigest md5 = MessageDigest.getInstance("MD5");
+                final byte[] digest = md5.digest(query.getBytes(Charset.forName("UTF-8")));
+                final String fileName = "./cache_" + Base64.getUrlEncoder().encodeToString(digest) + ".txt";
+                cacheFile = new File(fileName);
+                if (cacheFile.exists()) {
+                    final FileInputStream fStream = new FileInputStream(cacheFile.getAbsoluteFile());
+                    BufferedReader in = new BufferedReader(new InputStreamReader(fStream));
+                    StringBuilder contents = new StringBuilder();
+                    while (in.ready()) {
+                        contents.append(in.readLine());
+                    }
+                    JSONObject response = new JSONObject(contents.toString());
+                    return response.getJSONArray("elements");
+                }
+            } catch (NoSuchAlgorithmException | IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        JSONObject response = null;
         try {
             String rawResponse = getFromOverpass(fullQuery);
-            System.out.println(rawResponse);
-            //JSONObject f;
-            JSONParser parser = JSONFactory.instance().makeReader() new JSONParser();
-            parser.parse(rawResponse);
-        } catch (Exceptions.OverpassError e) {
-            e.printStackTrace();
-        } catch (IOException e) {
+            response = new JSONObject(rawResponse);
+
+            //write the data to cache
+            if(cachingEnabled && cacheFile != null) {
+                BufferedWriter writer = new BufferedWriter(new FileWriter(cacheFile));
+                writer.write(rawResponse);
+                writer.close();
+            }
+        } catch (Exceptions.OverpassError | IOException e) {
             e.printStackTrace();
         }
-        HashMap<String, String> response =  null;//json.loads(raw_response)
 
-        if(!response.containsKey("elements")) {
+        if(response == null || !response.has("elements")) {
             throw new Exceptions.UnknownOverpassError("Received an invalid answer from Overpass.");
         }
 
         if(!asGeoJSON) {
-            return response;
+            return response.getJSONArray("elements");
         } else {
-            return asGeoJSON(response.get("elements"));
+            return asGeoJSON(response.getJSONArray("elements"));
         }
     }
 
@@ -130,10 +154,6 @@ public class ApiClient {
         switch (status) {
             case HttpURLConnection.HTTP_OK:
                 InputStream response = connection.getInputStream();
-                Json.createParser(new StringReader("booger"));
-                JSONReader reader = new JSONReader(new InputStreamReader(response, "UTF-8"));
-
-
                 BufferedReader reader = new BufferedReader(new InputStreamReader(response, charset));
                 StringBuilder builder = new StringBuilder();
                 for (String line; (line = reader.readLine()) != null;) {
@@ -151,30 +171,45 @@ public class ApiClient {
         }
     }
 
-    private String asGeoJSON(String elements) {
+    private JSONArray asGeoJSON(JSONArray elements) {
         //print 'DEB _asGeoJson elements:', elements
 
-        /*features = []
-                for elem in elements:
-        elem_type = elem["type"]
-                if elem_type == "node":
-        geometry = geojson.Point((elem["lon"], elem["lat"]))
-        elif elem_type == "way":
-        points = []
-                for coords in elem["geometry"]:
-                points.append((coords["lon"], coords["lat"]))
-        geometry = geojson.LineString(points)
-                else:
-                continue
+        JSONArray features = new JSONArray();
+        JSONObject elem, outputElement;
+        int elemLength = elements.length();
+        for(int elemIdx=0;elemIdx<elemLength;elemIdx++) {
+            elem = elements.getJSONObject(elemIdx);
+            final String elemType = elem.getString("type");
+            outputElement = new JSONObject();
 
-        feature = geojson.Feature(
+            if(elemType.equals("node")) {
+                double[] coord = {elem.getDouble("lon"), elem.getDouble("lat")};
+
+            } else if (elemType.equals("way")) {
+                JSONArray elemPoints = elem.getJSONArray("geometry");
+                int elemPointLength = elemPoints.length();
+                double[][] points = new double[elemPointLength][2];
+
+                JSONObject coords;
+                for(int pointIdx = 0; pointIdx<elemPointLength;pointIdx++) {
+                    coords = elemPoints.getJSONObject(pointIdx);
+                    double[] coord = {coords.getDouble("lon"), coords.getDouble("lat")};
+                    points[pointIdx++] = coord;
+                }
+            } else {
+                continue;
+            }
+            features.put(elemIdx, outputElement);
+        }
+
+        /*feature = geojson.Feature(
         id=elem["id"],
         geometry=geometry,
         properties=elem.get("tags"))
                 features.append(feature)
 
         return geojson.FeatureCollection(features);*/
-        return null;
+        return features;
     }
 }
 
---
+++ src/Overpass/OverpassConverter.java
@@ -0,0 +1,134 @@
+package Overpass;
+
+import OSM.*;
+import com.sun.javaws.exceptions.InvalidArgumentException;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Iterator;
+
+/**
+ * Created by nick on 11/5/15.
+ */
+public class OverpassConverter {
+    private final static String WAY_QUERY_FORMAT = "(way%s(%.04f,%.04f,%.04f,%.04f);>);";
+    private OSMEntitySpace entitySpace;
+
+    public OSMEntitySpace getEntitySpace() {
+        return entitySpace;
+    }
+    public void fetchFromOverpass(OSMEntity entity, String overpassTagQuery, double boundingBoxPadding) throws InvalidArgumentException {
+        HashMap<String, String> apiConfig = new HashMap<>(1);
+        apiConfig.put("debug", "1");
+        ApiClient overpassClient = new ApiClient(null, apiConfig);
+        try {
+            Region boundingBox = entity.getBoundingBox().regionInset(-boundingBoxPadding, -boundingBoxPadding);
+            String query = String.format(WAY_QUERY_FORMAT, overpassTagQuery, boundingBox.origin.latitude, boundingBox.origin.longitude, boundingBox.extent.latitude, boundingBox.extent.longitude);
+
+            JSONArray elements = overpassClient.get(query, false);
+            final int elementLength = elements.length();
+
+            //init the entity space to contain the fetched OSM data
+            entitySpace = new OSMEntitySpace(elementLength);
+
+            //transmute the JSON data into OSM objects
+            final String keyTags = "tags", keyId = "id", keyVersion = "version";
+            JSONObject curElement;
+            for(int elemIdx=0;elemIdx<elementLength;elemIdx++) {
+                curElement = elements.getJSONObject(elemIdx);
+                final String elementType = curElement.getString("type");
+                if(elementType.equals(OSMEntity.OSMType.node.name())) {
+                    OSMNode node = new OSMNode(curElement.getLong(keyId));
+                    node.setCoordinate(curElement.getDouble("lat"), curElement.getDouble("lon"));
+
+                    if(curElement.has(keyTags)) {
+                        addTags(curElement, node);
+                    }
+                    if(curElement.has(keyVersion)) {
+                        addMetadata(curElement,node);
+                    }
+                    entitySpace.addEntity(node, OSMEntitySpace.EntityMergeStrategy.overwrite, null);
+                } else if(elementType.equals(OSMEntity.OSMType.way.name())) {
+                    OSMWay way = new OSMWay(curElement.getLong(keyId));
+                    if(curElement.has(keyTags)) {
+                        addTags(curElement, way);
+                    }
+                    if(curElement.has(keyVersion)) {
+                        addMetadata(curElement, way);
+                    }
+                    JSONArray wayNodes = curElement.getJSONArray("nodes");
+                    OSMNode curNode;
+                    for(int nodeIdx=0;nodeIdx<wayNodes.length();nodeIdx++) {
+                        long nodeId = wayNodes.getLong(nodeIdx);
+                        curNode = entitySpace.allNodes.get(nodeId);
+                        way.addNode(curNode);
+                    }
+                    entitySpace.addEntity(way, OSMEntitySpace.EntityMergeStrategy.overwrite, null);
+                } else if(elementType.equals(OSMEntity.OSMType.relation.name())) {
+                    System.out.println(curElement.toString());
+                    OSMRelation relation = new OSMRelation(curElement.getLong(keyId));
+                    if(curElement.has(keyTags)) {
+                        addTags(curElement, relation);
+                    }
+                    if(curElement.has(keyVersion)) {
+                        addMetadata(curElement, relation);
+                    }
+                    if(curElement.has("members")) { //NOTE: relations are last in the Overpass
+                        JSONArray members = curElement.getJSONArray("members");
+                        for(int memberIdx=0;memberIdx<members.length();memberIdx++) {
+                            JSONObject curMember = members.getJSONObject(memberIdx);
+                            final long memberId = curMember.getLong("ref");
+                            final String memberRole = curMember.getString("role");
+                            final String memberType = curMember.getString("type");
+
+                            //check if the member exists
+                            OSMEntity memberEntity = null;
+                            switch(memberType) {
+                                case "node":
+                                    memberEntity = entitySpace.allNodes.get(memberId);
+                                    if(memberEntity == null) {
+                                        memberEntity = OSMNode.create();
+                                    }
+                                    break;
+                                case "way":
+                                    memberEntity = entitySpace.allWays.get(memberId);
+                                    if(memberEntity == null) {
+                                        memberEntity = OSMWay.create();
+                                    }
+                                    break;
+                                case "relation":
+                                    memberEntity = entitySpace.allRelations.get(memberId);
+                                    if(memberEntity == null) {
+                                        memberEntity = OSMRelation.create();
+                                    }
+                                    break;
+                            }
+                            if(memberEntity != null) {
+                                relation.addMember(memberEntity, memberRole);
+                            }
+                        }
+                    }
+                    entitySpace.addEntity(relation, OSMEntitySpace.EntityMergeStrategy.overwrite, null);
+                }
+            }
+        } catch (Exceptions.UnknownOverpassError unknownOverpassError) {
+            unknownOverpassError.printStackTrace();
+        }
+    }
+    private static void addTags(JSONObject element, OSMEntity entity) {
+        JSONObject tags = element.getJSONObject("tags");
+        Iterator<String> iterator = tags.keys();
+        while (iterator.hasNext()) {
+            final String key = iterator.next();
+            entity.setTag(key, tags.getString(key));
+        }
+    }
+    private static void addMetadata(JSONObject element, OSMEntity entity) {
+        entity.uid = element.getInt("uid");
+        entity.user = element.getString("user");
+        entity.changeset = element.getInt("changeset");
+        entity.version = element.getInt("version");
+        entity.timestamp = element.getString("timestamp");
+    }
+}
